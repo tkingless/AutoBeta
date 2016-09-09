402c179a9c9552faac27ae9f7d0ea07df7d38753
tsangkk <t.kingless@yahoo.com>
2016-09-09 10:51:28 +0800

    add install, setup and selenium jar

---
+++ InstallEnv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+npm install mocha chai webdriverjs phantomjs
---
+++ SetEnv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SetEnv
+#ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo $ABSOLUTE_PATH
+
+PATH="$ABSOLUTE_PATH/node_modules/phantomjs/bin:$PATH" 
+
+#run selenium server
+java -jar selenium-server-standalone-2.53.1.jar
