3e13d2512b719523fd99359e0ab27956be8cf8fb
tsangkk <t.kingless@yahoo.com>
2016-09-09 17:11:03 +0800

    update callback and setting examples

--- PhantomJSexamples/codeEvaluation.js
+++ PhantomJSexamples/codeEvaluation.js
@@ -1,9 +1,18 @@
 var page = require('webpage').create();
+
+//define callbacks
 page.onConsoleMessage = function(msg) {
   console.log('Page title is ' + msg);
 };
+page.onResourceRequested = function(request) {
+  console.log('Request ' + JSON.stringify(request, undefined, 4));
+};
+page.onResourceReceived = function(response) {
+  console.log('Receive ' + JSON.stringify(response, undefined, 4));
+};
 
 var url = 'http://www.google.com';
+page.settings.userAgent = '"Mozilla/5.0';
 
 //TODO url as list, page loop open
 
