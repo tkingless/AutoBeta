9aa4655876b813a79415e697a414a1098ddbdf6a
tsangkk <t.kingless@yahoo.com>
2016-09-09 15:48:19 +0800

    capture screenshot

--- PhantomJSexamples/pageLoading.js
+++ PhantomJSexamples/pageLoading.js
@@ -2,8 +2,7 @@ var httpPage = require('webpage').create();
 httpPage.open('http://edx.eduhk.hk', function(status) {
 	console.log("Status: " + status);
 	if (status === "success") {
-		//TODO this is not working
-		//httpPage.render('/static/images/logo.7eec09d43a49.png');
+		httpPage.render('screenshotCaptured.png');
 	}
 	phantom.exit();
 });
