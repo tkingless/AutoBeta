46cc57cba5b4c59c9fd7c0a176f492cea52b8488
tsangkk <t.kingless@yahoo.com>
2016-09-09 15:32:29 +0800

    formated

--- PhantomJSexamples/pageLoading.js
+++ PhantomJSexamples/pageLoading.js
@@ -1,9 +1,10 @@
-var page = require('webpage').create();
-page.open('http://edx.eduhk.hk', function(status) {
-  console.log("Status: " + status);
-  if(status === "success") {
-    page.render('/static/images/logo.7eec09d43a49.png');
-  }
-  phantom.exit();
+var httpPage = require('webpage').create();
+httpPage.open('http://edx.eduhk.hk', function(status) {
+	console.log("Status: " + status);
+	if (status === "success") {
+		//TODO this is not working
+		//httpPage
+	.render('/static/images/logo.7eec09d43a49.png');
+	}
+	phantom.exit();
 });
-
