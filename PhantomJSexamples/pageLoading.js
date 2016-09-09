9b672a07af303772ae91ba989f942045b679c01b
tsangkk <t.kingless@yahoo.com>
2016-09-09 15:53:43 +0800

    can capture https

--- PhantomJSexamples/pageLoading.js
+++ PhantomJSexamples/pageLoading.js
@@ -4,5 +4,16 @@ httpPage.open('http://edx.eduhk.hk', function(status) {
 	if (status === "success") {
 		httpPage.render('screenshotCaptured.png');
 	}
+	//phantom.exit();
+});
+
+//node_modules/phantomjs/bin/phantomjs --ssl-protocol=any PhantomJSexamples/pageLoading.js
+
+var httpsPage = require('webpage').create();
+httpsPage.open('https://hk.yahoo.com/', function(status) {
+	console.log("Status: " + status);
+	if (status === "success") {
+		httpsPage.render('screenshotCapturedHttps.png');
+	}
 	phantom.exit();
 });
