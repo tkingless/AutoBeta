0a068a9620cc41bb46e0147fe01367d20bc138f9
tsangkk <t.kingless@yahoo.com>
2016-09-09 15:34:05 +0800

    debug

--- PhantomJSexamples/pageLoading.js
+++ PhantomJSexamples/pageLoading.js
@@ -3,8 +3,7 @@ httpPage.open('http://edx.eduhk.hk', function(status) {
 	console.log("Status: " + status);
 	if (status === "success") {
 		//TODO this is not working
-		//httpPage
-	.render('/static/images/logo.7eec09d43a49.png');
+		//httpPage.render('/static/images/logo.7eec09d43a49.png');
 	}
 	phantom.exit();
 });
