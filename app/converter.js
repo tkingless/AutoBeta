973f491340b319bd4329600997603b66339ae974
tsangkk <t.kingless@yahoo.com>
2016-09-14 13:47:49 +0800

    update test

--- app/converter.js
+++ app/converter.js
@@ -11,3 +11,13 @@ exports.rgbToHex = function(red, green, blue) {
 function pad(hex) {
   return (hex.length === 1 ? "0" + hex : hex);
 }
+
+exports.hexToRgb = function(hex) {
+
+  var red   = parseInt(hex.substring(0, 2), 16);
+  var green = parseInt(hex.substring(2, 4), 16);
+  var blue  = parseInt(hex.substring(4, 6), 16);
+
+  return [red, green, blue];
+
+};
