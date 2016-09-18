545001be690d2fac37d084802817e2e84db7664f
tkingless <unstashworld@gmail.com>
2016-09-18 16:52:44 +0800

    remove old style things

--- RunEnv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#run selenium server
-java -jar selenium-server-standalone-2.53.1.jar
-
-#rune node server
-#node app/server.js
--- SetEnv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SetEnv
-#ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
-ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-echo $ABSOLUTE_PATH
-
-PATH="$ABSOLUTE_PATH/node_modules/phantomjs/bin:$PATH" 
