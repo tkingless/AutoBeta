f384e27182729805a6c3af25144764f014caac00
tkingless <unstashworld@gmail.com>
2016-09-18 22:13:23 +0800

    add test cases for standalone

---
+++ tests/wdio/specs/demo.mocha.phantomjs.js
@@ -0,0 +1,16 @@
+'use strict'
+const assert = require('assert')
+
+module.exports = () => {
+
+  describe('First Test Group', () => {
+    it('gets the title of MDN toppage', () => {
+      browser
+        .url('https://developer.mozilla.org/en-US/')
+        .getTitle().then(title => {
+           assert.equal(title, 'Mozilla Developer Network')
+        })
+    })
+  })
+
+}
