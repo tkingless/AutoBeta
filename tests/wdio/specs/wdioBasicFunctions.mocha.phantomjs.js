625306c7372a3a08258b15539f4c7c39b10d47d7
tsangkk <t.kingless@yahoo.com>
2016-09-19 12:29:41 +0800

    update workable basic test cases

--- tests/wdio/specs/wdioBasicFunctions.mocha.phantomjs.js
+++ tests/wdio/specs/wdioBasicFunctions.mocha.phantomjs.js
@@ -23,17 +23,53 @@ module.exports = () => {
 	})*/
 
 	describe('Webdriverio basic functionalities', function() {
+
 		before(function() {
 			chaiAsPromised.transferPromiseness = browser.transferPromiseness;
 			mkdirSync('./tests/wdio/wdioBasicFunctions/');
-			
 			return browser.url('http://www.google.com');
 		});
 
-		it('Transfer promises, taking screenshot on google', function() {
-			//var screenshot = browser.saveScreenshot();
-			//fs.writeFileSync('./myShort.png', screenshot);
-			browser.saveScreenshot('./tests/wdio/wdioBasicFunctions/snapshot.png');
+		describe('Take screenshot', function() {
+
+			//all it()s are run in series, with only mocha done() callback is called , jump to next it(), if no done event passed to it(), the
+			//it() will just go straight away not waiting async function to fisnish, so add done() to when asyn function is completed.
+
+			it('Transfer promises, taking screenshot on google', function(done) {
+
+				browser.saveScreenshot('./tests/wdio/wdioBasicFunctions/google.defaultSize.png');
+				done();
+
+			});
+
+			it('Set viewPortSize and taking another screenshot', function(done) {
+
+				var desiredSize = {
+					width: 1024,
+					height: 768
+				};
+
+				browser.setViewportSize(desiredSize).then(function() {
+					browser.getViewportSize().then(function(size) {
+
+						var screenshot = browser.saveScreenshot().then(function(data) {
+							console.log('browser save 1024 768 png');
+							return fs.writeFileSync('./tests/wdio/wdioBasicFunctions/google.1024.768.png', data);
+						})
+						done(); //you may try putting this done() on the last line if this it()
+
+					})
+				});
+
+			});
+
+			it('Get viewPortSize', function(done) {
+				browser.getViewportSize().then(function(size) {
+					console.log(size) // outputs: {width: 1024, height: 768}
+					done();
+				});
+			});
+
 		});
 
 	});
@@ -41,11 +77,11 @@ module.exports = () => {
 }
 
 
-//TODO make this function as utility
-function mkdirSync (path) {
-  try {
-    fs.mkdirSync(path);
-  } catch(e) {
-    if ( e.code != 'EEXIST' ) throw e;
-  }
+//TODO export this function to utility
+function mkdirSync(path) {
+	try {
+		fs.mkdirSync(path);
+	} catch (e) {
+		if (e.code != 'EEXIST') throw e;
+	}
 }
