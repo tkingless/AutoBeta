8386e91d04090f0374425846e321b6fe6be9993d
tsangkk <t.kingless@yahoo.com>
2016-09-19 10:05:43 +0800

    update test case: screenshot

--- tests/wdio/specs/wdioBasicFunctions.mocha.phantomjs.js
+++ tests/wdio/specs/wdioBasicFunctions.mocha.phantomjs.js
@@ -1,10 +1,16 @@
 'use strict'
 const assert = require('assert'),
-	fs = require('fs')
+	fs = require('fs'),
+	chai = require('chai'),
+	chaiAsPromised = require('chai-as-promised')
+
+chai.Should();
+chai.use(chaiAsPromised);
 
 module.exports = () => {
 
-	describe('Webdriverio basic functionalities', () => {
+	//not synchronous
+	/*describe('Webdriverio basic functionalities', () => {
 		it('Take screenshot on www.google.com', () => {
 			browser
 				.url('http://www.google.com').then(function() {
@@ -14,6 +20,32 @@ module.exports = () => {
 			fs.writeFileSync('./myShort.png', screenshot);
 			browser.saveScreenshot('./snapshot.png');
 		})
-	})
+	})*/
 
+	describe('Webdriverio basic functionalities', function() {
+		before(function() {
+			chaiAsPromised.transferPromiseness = browser.transferPromiseness;
+			mkdirSync('./tests/wdio/wdioBasicFunctions/');
+			
+			return browser.url('http://www.google.com');
+		});
+
+		it('Transfer promises, taking screenshot on google', function() {
+			//var screenshot = browser.saveScreenshot();
+			//fs.writeFileSync('./myShort.png', screenshot);
+			browser.saveScreenshot('./tests/wdio/wdioBasicFunctions/snapshot.png');
+		});
+
+	});
+
+}
+
+
+//TODO make this function as utility
+function mkdirSync (path) {
+  try {
+    fs.mkdirSync(path);
+  } catch(e) {
+    if ( e.code != 'EEXIST' ) throw e;
+  }
 }
