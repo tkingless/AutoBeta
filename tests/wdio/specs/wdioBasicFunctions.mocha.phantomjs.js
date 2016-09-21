394b1b3a155474959f450d4230d74922c9f773f8
tkingless <unstashworld@gmail.com>
2016-09-21 21:50:32 +0800

    reformat

--- tests/wdio/specs/wdioBasicFunctions.mocha.phantomjs.js
+++ tests/wdio/specs/wdioBasicFunctions.mocha.phantomjs.js
@@ -10,79 +10,80 @@ chai.should();
 
 module.exports = () => {
 
-	//not synchronous
-	/*describe('Webdriverio basic functionalities', () => {
-		it('Take screenshot on www.google.com', () => {
-			browser
-				.url('http://www.google.com').then(function() {
-
-				})
-			var screenshot = browser.saveScreenshot();
-			fs.writeFileSync('./myShort.png', screenshot);
-			browser.saveScreenshot('./snapshot.png');
-		})
-	})*/
-
-	describe('Webdriverio basic functionalities', function() {
-
-		before(function() {
-			chaiAsPromised.transferPromiseness = browser.transferPromiseness;
-			return browser.url('http://www.google.com');
-		});
-
-		describe('Take screenshot', function() {
-
-			//all it()s are run in series, with only mocha done() callback is called , jump to next it(), if no done event passed to it(), the
-			//it() will just go straight away not waiting async function to fisnish, so add done() to when asyn function is completed.
-
-			it('Transfer promises, taking screenshot on google', function(done) {
-
-				mkdirSync('./tests/wdio/wdioBasicFunctions/');
-				browser.saveScreenshot('./tests/wdio/wdioBasicFunctions/google.defaultSize.png');
-				done();
-
-			});
-
-			it('Set viewPortSize and taking another screenshot', function(done) {
-
-				var desiredSize = {
-					width: 1024,
-					height: 768
-				};
-
-				browser.setViewportSize(desiredSize).then(function() {
-					browser.getViewportSize().then(function(size) {
-
-						var screenshot = browser.saveScreenshot().then(function(data) {
-							console.log('browser save 1024 768 png');
-							return fs.writeFileSync('./tests/wdio/wdioBasicFunctions/google.1024.768.png', data);
-						})
-						done(); //you may try putting this done() on the last line if this it()
+		//not synchronous
+		/*describe('Webdriverio basic functionalities', () => {
+			it('Take screenshot on www.google.com', () => {
+				browser
+					.url('http://www.google.com').then(function() {
 
 					})
+				var screenshot = browser.saveScreenshot();
+				fs.writeFileSync('./myShort.png', screenshot);
+				browser.saveScreenshot('./snapshot.png');
+			})
+		})*/
+
+		describe('Webdriverio basic functionalities', function() {
+
+				before(function() {
+					chaiAsPromised.transferPromiseness = browser.transferPromiseness;
+					return browser.url('http://www.google.com');
 				});
 
-			});
+				describe('Take screenshot', function() {
 
-			it('Get viewPortSize', function(done) {
-				browser.getViewportSize().should.eventually.deep.equal({
-					width: 1024,
-					height: 768
-				}).notify(done);
-			});
+					//all it()s are run in series, with only mocha done() callback is called , jump to next it(), if no done event passed to it(), the
+					//it() will just go straight away not waiting async function to fisnish, so add done() to when asyn function is completed.
 
-		});
+					it('Transfer promises, taking screenshot on google', function(done) {
 
-	});
+						mkdirSync('./tests/wdio/wdioBasicFunctions/');
+						browser.saveScreenshot('./tests/wdio/wdioBasicFunctions/google.defaultSize.png');
+						done();
+					});
 
-}
+					it('Set viewPortSize and taking another screenshot', function(done) {
+
+						var desiredSize = {
+							width: 1024,
+							height: 768
+						};
+
+						browser.setViewportSize(desiredSize).then(function() {
+							browser.getViewportSize().then(function(size) {
+
+								var screenshot = browser.saveScreenshot().then(function(data) {
+									console.log('browser save 1024 768 png');
+									return fs.writeFileSync('./tests/wdio/wdioBasicFunctions/google.1024.768.png', data);
+								})
+								done(); //you may try putting this done() on the last line if this it()
+							})
+						});
+					});
+
+					it('Get viewPortSize', function(done) {
+						browser.getViewportSize().should.eventually.deep.equal({
+							width: 1024,
+							height: 768
+						}).notify(done);
+					});
+				});
+
+				describe('Access  Webdriverio website', function() {
 
 
-//TODO export this function to utility
-function mkdirSync(path) {
-	try {
-		fs.mkdirSync(path);
-	} catch (e) {
-		if (e.code != 'EEXIST') throw e;
-	}
-}
+					}
+
+				});
+
+		}
+
+
+		//TODO export this function to utility
+		function mkdirSync(path) {
+			try {
+				fs.mkdirSync(path);
+			} catch (e) {
+				if (e.code != 'EEXIST') throw e;
+			}
+		}
