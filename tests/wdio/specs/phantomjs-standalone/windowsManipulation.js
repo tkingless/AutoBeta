32a849ce121db7326ebc5898d2e9229dd115fca6
tsangkk <t.kingless@yahoo.com>
2016-09-26 14:39:32 +0800

    update window test case

--- tests/wdio/specs/phantomjs-standalone/windowsManipulation.js
+++ tests/wdio/specs/phantomjs-standalone/windowsManipulation.js
@@ -27,7 +27,7 @@ module.exports = () => {
 
 		describe('Basic Windows functions', function() {
 
-			var firstWin;
+			var Wnds = []; //delcaring Wnds as array, otherwise no push() available
 			it('Get current windows handles', function() {
 				return browser.windowHandles().then(function(windowHdls) {
 					var windows = windowHdls.value;
@@ -37,12 +37,12 @@ module.exports = () => {
 					windows.length.should.equal(1);
 					Array.isArray(windows).should.equal(true);
 					browser.saveScreenshot(testSuiteBaseDir.concat('1.first.windows.png'));
-					firstWin = windows[0];
+					Wnds.push(windows[0]);
 				})
 			});
 
 			it('Create new window', function() {
-				//TOCHECK: For phantomjs standalone, newWindow(url), url input not working, still need to use url
+				//TOCHECK: For phantomjs standalone, newWindow(url), url input not working, still need to use url()
 				return browser.newWindow('http://whatever.com.you.input.is.useless').getUrl().then(function(url) {
 						url.should.equal('about:blank');
 					}).url('http://example.com')
@@ -51,15 +51,26 @@ module.exports = () => {
 							var windows = windowHdls.value;
 							windows.length.should.equal(2);
 							browser.saveScreenshot(testSuiteBaseDir.concat('2.second.windows.exampleDotCom.png'));
+							Wnds.push(windows[1]);
 						})
 					})
 
 			})
 
 			it('Switching back to first Windows', function(){
-				return browser.switchTab(firstWin).then(() => {
-					browser.saveScreenshot(testSuiteBaseDir.concat('3.backTo.firstWin.png'));
+				return browser.switchTab(Wnds).then(() => {
+					browser.saveScreenshot(testSuiteBaseDir.concat('3.backTo.FirstWin.png'));
 				});
+			})
+
+			it('Close inActive window', function(){
+				//you cannot close current window directly, workaround switch then close
+				return browser.close(Wnds[1]).then(function(){
+					browser.saveScreenshot(testSuiteBaseDir.concat('4.closeSecondWin.png'));
+					return browser.getTabIds().then(function(wnds){
+						wnds.length.should.equal(1);
+					})
+				})
 			})
 
 		});
